599f504ac30d1f2472d98a062436259fdf2c43d1
Vasiliy <nvasya95@gmail.com>
2019-03-08 12:58:55 +0200

    Share article

--- app/src/main/java/com/vjettest/news/article/ArticleActivity.kt
+++ app/src/main/java/com/vjettest/news/article/ArticleActivity.kt
@@ -3,8 +3,11 @@ package com.vjettest.news.article
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.widget.ShareActionProvider
+import androidx.core.view.MenuItemCompat
 import com.vjettest.news.R
 import com.vjettest.news.common.AppBaseActivity
 import com.vjettest.news.common.formatRelative
@@ -33,10 +36,21 @@ class ArticleActivity : AppBaseActivity() {
         super.onPostCreate(savedInstanceState)
 
         title = article.title
-        textViewDescription.text = article.description
+        textViewDescription.text = article.description ?: article.title
         textViewDate.text = article.publishedAt.formatRelative(this)
         textViewContent.text = article.content
         imageView.setImageAsync(article.urlToImage)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.options_article, menu)
+        val item = menu.findItem(R.id.menu_item_share)
+        val shareProvider = MenuItemCompat.getActionProvider(item) as ShareActionProvider
+        shareProvider.setShareIntent(Intent(Intent.ACTION_SEND).apply {
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TEXT, "${article.title} - ${article.url}")
+        })
+        return super.onCreateOptionsMenu(menu)
     }
 
     companion object {
